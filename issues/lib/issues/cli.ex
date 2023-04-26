3c3686c525590303ebda01f6ddc8206c2d2ddcb6
Park Sang Yeop <81801012+lotus0204@users.noreply.github.com>
2023-04-26 22:11:48 +0900

    13.9 데이터 정렬

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -46,11 +46,17 @@ defmodule Issues.CLI do
   def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_ascending_order()
   end
 
   def decode_response({:ok, body}), do: body
   def decode_response({:error, error}) do
     IO.puts "Error fetching from Github: #{error["message"]}"
     System.halt(2)
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 -> i1["created_at"] >= i2["created_at"] end)
   end
 end
--- issues/test/cli_test.exs
+++ issues/test/cli_test.exs
@@ -2,7 +2,7 @@ defmodule CliTest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.CLI, only: [ parse_args: 1]
+  import Issues.CLI, only: [ parse_args: 1, sort_into_ascending_order: 1]
 
   test "-h나 --help가 옵션으로 파싱되면 :help가 반환된다." do
     assert parse_args(["-h", "anything"]) == :help
@@ -15,5 +15,16 @@ defmodule CliTest do
 
   test "값을 2개 전달하면 개수에 기본값을 사용한다." do
     assert parse_args(["user", "project"]) == {"user", "project", 4}
+  end
+
+  test "내림차순 정렬이 잘 수행된다." do
+    result = sort_into_ascending_order(fake_created_at_list(["c", "a", "b"]))
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+    assert issues == ~w{c b a}
+  end
+
+  defp fake_created_at_list(values) do
+    for value <- values,
+    do: %{"created_at" => value, "other_data" => "xxx"}
   end
 end
