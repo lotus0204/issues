2f53f8dda36bef9c9162dcad2cfef5156c19b909
Park Sang Yeop <81801012+lotus0204@users.noreply.github.com>
2023-05-14 17:36:16 +0900

    otp-server, supervisor

---
+++ otp-server/1/sequence/lib/sequence/server.ex
@@ -0,0 +1,24 @@
+defmodule Sequence.Server do
+  use GenServer
+
+  #생성자 역할을하는 코드
+  def init(initial_number) do
+    {:ok, initial_number}
+  end
+
+  def handle_call(:next_number, _from, current_number) do
+    {:reply, current_number, current_number + 1}
+  end
+
+  def handle_cast({:increment_number, delta}, current_number) do
+    {:noreply, current_number + delta}
+  end
+
+  # def init(stack) do
+  #   {:ok, stack}
+  # end
+
+  # def handle_call(:pop, _from, [head | tail]) do
+  #   {:reply, head, tail}
+  # end
+end
